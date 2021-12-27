d077ec603ae4a9cabf561fdd3a2d36cdc34793a1
LakkappaBJ <lucky.xworkz@gmail.com>
2021-12-27 22:35:11 +0530

    Factorial Series

---
+++ FactSeries.java
@@ -0,0 +1,11 @@
+//To print factorial series..
+class FactSeries{
+	public static void main(String []args){ 
+	int fact=1,n=10;
+	
+			for(int i=1;i<=n;i++){
+					fact=fact*i;
+			System.out.println(i+"!="+fact);
+			}
+	}
+}
