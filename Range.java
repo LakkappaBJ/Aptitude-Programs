ac4423a23d52c05a58b0b39e5f139898118f17aa
Lakkappa Bhajantri <lucky.xworkz@gmail.com>
2022-01-19 19:09:55 +0530

    range od datatypes
    to check range for primitive datatypes using wrapper calss
    Byte, Integer, Short, Long, Float, Double

---
+++ Range.java
@@ -0,0 +1,17 @@
+//To calculate range of datatypes
+class Range{
+	public static void main(String args[]){
+	System.out.println(Byte.MAX_VALUE); //Byte wrapper class
+	System.out.println(Byte.MIN_VALUE);
+	System.out.println(Short.MAX_VALUE); //Short wrapper class
+	System.out.println(Short.MIN_VALUE);
+	System.out.println(Integer.MAX_VALUE); //Integer wrapper class
+	System.out.println(Integer.MIN_VALUE);
+	System.out.println(Long.MAX_VALUE); //Long wrapper class
+	System.out.println(Long.MIN_VALUE);
+	System.out.println(Float.MAX_VALUE);  //Float wrapper class
+	System.out.println(Float.MIN_VALUE);
+	System.out.println(Double.MAX_VALUE); // Double wrapper class
+	System.out.println(Double.MIN_VALUE);
+	}
+}
