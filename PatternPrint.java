f40d00705819ac2d3a309e44e6850ff2623e0017
Lakkappa Bhajantri <lucky.xworkz@gmail.com>
2022-01-26 14:50:26 +0530

    using command line arguments

---
+++ PatternPrint.java
@@ -0,0 +1,23 @@
+class PatternPrint{
+	public static void main(String args[]){
+		/* for(int i=1;i<=6;i++){
+			System.out.print(" ");
+			for(int j=1;j<=i;j++){  // imp{j<=i}
+				System.out.print(j+" ");
+			}
+		System.out.println(); */
+		
+		/* for(int i=1;i<=5;i++){
+			for(int j=1;j<=5;j++){
+				System.out.print("* ");
+			}
+		System.out.println();
+		} */
+		for(int i=1;i<=5;i++){
+			for(int j=1;j<=i;j++){
+				System.out.print("* ");
+			}
+		System.out.println();
+		}	
+	}
+}
