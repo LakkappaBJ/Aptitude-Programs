7ad30415af93eb1abd0da394f815bdec2d27b6d2
Lakkappa Bhajantri <lucky.xworkz@gmail.com>
2022-01-26 14:51:33 +0530

    check given input character is vowel or consonant

---
+++ CheckVowel.java
@@ -0,0 +1,31 @@
+//Write a program to check given character is vowel or not.
+class CheckVowel{
+	public static void main(String args[]){
+		char c;
+		c='c';
+	 if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u'){
+			System.out.println("This is Vowel");
+		}
+		else {
+			System.out.println("This is Consonant");
+		}
+	 
+		/* if(c=='a'){
+			System.out.println("Vowel");
+		}
+		if(c=='e'){
+			System.out.println("Vowel");
+		}
+		if(c=='i'){
+			System.out.println("Vowel");
+		}
+		if(c=='o'){
+			System.out.println("Vowel");
+		}
+		if(c=='u'){
+			System.out.println("Vowel");
+		}
+		else 
+			System.out.println("Consonant"); */
+	}
+}
